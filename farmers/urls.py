1d905417d1efd411334bed1ac065d79ab2bf16c0
uttammagaju <uttammagaju0000@gmail.com>
2023-07-05 18:16:25 +0545

    small update in employee and working in farmer

--- farmers/urls.py
+++ farmers/urls.py
@@ -6,10 +6,10 @@ from . import views
 app_name = "farmers"
 
 urlpatterns = [
-    path("", views.FarmerHomeView, name="home"),
-    # payment
-    path("payment/due", views.paymentDue, name = "payment-due"),
-    path("payment/paid", views.paymentPaid, name = "payment-paid"),
+    # path("", views.FarmerHomeView, name="home"),
+    # # payment
+    # path("payment/due", views.paymentDue, name = "payment-due"),
+    # path("payment/paid", views.paymentPaid, name = "payment-paid"),
 
 
 ]
